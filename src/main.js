41e8038c1af79ce805145824ba8cadd93a90b85c
Rafaela <study.program67@gmail.com>
2022-01-25 23:48:44 -0400

    delete folders

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import GenerateCPF from './modules/GenerateCPF';
-import './assets/css/style.css';
-
-(function() {
-  const generate = new GenerateCPF;
-  const generatedCpf = document.querySelector('.result');
-
-  generatedCpf.innerHTML = generate.generateNewCPF();
-})();
